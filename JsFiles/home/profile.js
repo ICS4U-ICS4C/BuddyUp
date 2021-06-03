4ee174bfd4f04e288ef4e1355cb97207f220a39b
743627 <54897138+743627@users.noreply.github.com>
2021-06-03 13:45:33 -0400

    added preview profile

---
+++ JsFiles/home/profile.js
@@ -0,0 +1,16 @@
+let file = {}
+//SignUp function
+$('.picture').change(function(){
+  const preview = document.querySelector('.circle');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", function () {
+    // convert image file to base64 string
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+})
