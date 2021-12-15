36314ea0ce47470ce7555bdb67b343efe1e5c869
743627 <54897138+743627@users.noreply.github.com>
2021-12-15 10:09:43 -0500

    Update check.js

--- JsFiles/check.js
+++ JsFiles/check.js
@@ -5,8 +5,8 @@ window.onload = async function(){
           await firebase.database().ref(`Users/${firebase.auth().currentUser.uid}/online`).set('true')
         }else{
           // User not logged in or has just logged out.
-          window.location = '../index.html'
-          firebase.database().ref(sessionStorage.getItem('member')).remove()
+//           window.location = '../index.html'
+//           firebase.database().ref(sessionStorage.getItem('member')).remove()
         }
     });
   },1500)
